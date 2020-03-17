e9e0959cf1c7e8274c149cce8b971d55b24b579e
Mateusz Gliwka <gliwson@gmail.com>
2020-03-17 01:19:02 +0100

    init

--- src/main/java/pl/javastart/controller/HomeController.java
+++ src/main/java/pl/javastart/controller/HomeController.java
@@ -2,6 +2,7 @@ package pl.javastart.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,13 +22,13 @@ public class HomeController {
     }
 
 
-    @RequestMapping("/wirus")
+    @GetMapping("/wirus")
     public String getMap(Model model) {
         model.addAttribute("pointList", dataRepo.getPointList());
         return "index";
     }
 
-    @RequestMapping("/wirus2")
+    @GetMapping("/wirus2")
     public String getMa2p(Model model) {
         model.addAttribute("pointList", dataRepo.getPointList());
         return "index";
