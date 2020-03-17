3e46aa16a719bda30e4fce4356201335e725deea
Mateusz Gliwka <gliwson@gmail.com>
2020-03-17 01:30:24 +0100

    init

--- src/main/java/pl/javastart/SpringDeployCargoApplication.java
+++ src/main/java/pl/javastart/SpringDeployCargoApplication.java
@@ -1,23 +1,16 @@
 package pl.javastart;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import pl.javastart.controller.PrintingRunnable;
 
 @SpringBootApplication
-public class SpringDeployCargoApplication extends SpringBootServletInitializer {
+public class SpringDeployCargoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDeployCargoApplication.class, args);
 
 		Thread thread = new Thread(new PrintingRunnable( 60));
 		thread.start();
-	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
 	}
 }
